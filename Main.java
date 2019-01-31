1b21208d616fb8c90784d42f346b054a08251a08
Ayush Ranjan <35203726+ayushranjan01@users.noreply.github.com>
2019-01-31 11:36:02 -0500

    Create Main.java

---
+++ Main.java
@@ -0,0 +1,12 @@
+public class Main {
+  public static void main(String[] args) {
+    SmartCar myCar = new SmartCar();
+    myCar.startUp();
+    myCar.drive(4);
+    myCar.chargeCar(12);
+    myCar.drive(5);
+    myCar.checkcarBattery();
+  
+	}
+//calling the methods to see how they work
+}
